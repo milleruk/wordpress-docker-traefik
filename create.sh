3667631ef2efeca24ce040dc5de501c0d6e15281
miller <ben@haresign.net>
2019-08-02 22:07:36 +0000

    Update 'create.sh'

--- create.sh
+++ create.sh
@@ -1,15 +1,22 @@
+#!/bin/bash
 clear
 read -r -p "What is the DOMAIN Name for this wordpress? " DOMAIN
-read -r -p "What is the SUB-DOMAIN Name for this wordpress? " SUBDOMAIN
-read -r -p "MYSQL Hostname? " DB_HOST
+read -r -p "What is the SUB-DOMAIN Name for this wordpress? (By default 'www' will be used if no subdomain is selected). " SUBDOMAIN
+read -r -p "MYSQL Hostname? (By default 'localhost' will be used.) " DB_HOST
 read -r -p "MYSQL Username? " DB_USER
 read -r -p "MYSQL Database? " DB_NAME
 read -r -p "MYSQL Password? " DB_PASSWORD
 read -r -p "What network to add onto TRAEFIK? " TNETWORK
 
+if [ -z "$SUBDOMAIN" ]; then
+   SUBDOMAIN=www
+fi
+if [ -z "$DB_HOST" ]; then
+   DB_HOST=localhost
+fi
 
 
-echo "Creating the .env file with the values we have gathered"
+echo "$(tput setaf 1)Creating the .env file with the values we have gathered"
 printf "\\n"
 cat << EOF > .env
 EOF
@@ -24,3 +31,15 @@ echo "TNETWORK=$TNETWORK"
 } >> .env
 echo ".env file creation complete"
 printf "\\n\\n"
+
+# Download & Launch the containers
+echo "The containers will now be pulled and launched"
+echo "This may take a while depending on your download speed"
+read -r -p "Press any key to continue... " -n1 -s
+printf "\\n\\n"
+docker-compose up -d
+printf "\\n\\n"
+
+
+printf "Setup Complete - Open a browser and go to: \\n\\n"
+printf "https://"$SUBDOMAIN"."$DOMAIN"\\n\\n"
